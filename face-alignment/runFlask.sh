b17c31b2ce526e07bd3a40320737d8683c8d45b9
mosheor <ormoshe2204@gmail.com>
2018-07-11 19:17:38 +0300

    flask is working on fedoragit commit -m flask working on fedora!

--- face-alignment/runFlask.sh
+++ face-alignment/runFlask.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-#sudo docker kill FlaskServer
-#sudo docker rm FlaskServer
-#sudo docker image rm flask_server_image
-sudo docker build -t flask_server_image .
-sudo docker run -d -p 5000:5000 --name FlaskServer \
+#docker kill FlaskServer
+#docker rm FlaskServer
+#docker image rm flask_server_image
+docker build -t flask_server_image .
+docker run -p 5000:5000 --name FlaskServer \
  -e OBJ_STORE_IP=172.17.0.2 \
  -e KEY_ID='1PU0GPZJPE62AEO3NOAD' \
  -e SECRET_KEY='U3MOCBYhyYRCZdKW4FaBM57A33aVnir63HcHdTAh' \
