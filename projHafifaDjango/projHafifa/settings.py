8640febc2588f92f22266e3a3b0578711e7d32b6
mosheor <ormoshe2204@gmail.com>
2018-07-15 16:22:15 +0300

    fixed allowd hosts

--- projHafifaDjango/projHafifa/settings.py
+++ projHafifaDjango/projHafifa/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1+5=tx0qvfps7wn5$5@@y6r3u@j$2m+!0nf4%ut$igwnjfpba-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
